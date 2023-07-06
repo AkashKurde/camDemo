c43141e573b8bc94521135177d49bd66e126ef27
Akash Kurudekar/LGSIA HAkash.kurudekarA Device Solution(akash.kurudekar@lge.com) <akash.kurudekar@lge.com>
2023-07-06 11:33:30 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';
+import Webcam from 'react-webcam';
+import plupload from 'plupload';
+import axios from 'axios'
+const App = () => {
+  const webcamRef = useRef(null);
+  const [capturedImage, setCapturedImage] = useState(null);
+  const [facingMode, setFacingMode] = useState('user');
 
-function App() {
+  const captureImage = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setCapturedImage(imageSrc);
+  };
+
+  const switchCamera = () => {
+    setFacingMode((prevFacingMode) =>
+      prevFacingMode === 'user' ? 'environment' : 'user'
+    );
+  };
+
+  const handleUpload = () => {
+    // if (capturedImage) {
+
+
+    //   const formData = new FormData();
+    //   formData.append('file', file);
+    //   formData.append('name', 'capturedImage.png');
+    //   formData.append('empName', 'akash');
+    //   formData.append('empId', 'SI006423');
+    //   formData.append('category', 'bowl');
+    //   formData.append('chunks', 1);
+    //   formData.append('chunk', 0);
+
+
+
+    //   axios
+    //     .post('http://10.175.206.38:3038/api/image-upload', formData, {
+    //       headers: {
+    //         'Content-Type': 'multipart/form-data',
+    //       },
+    //     })
+    //     .then((response) => {
+    //       console.log('Image uploaded successfully:', response.data);
+    //       // Perform any additional actions after successful upload
+    //     })
+    //     .catch((error) => {
+    //       console.error('Error uploading image:', error);
+    //       // Handle upload error
+    //     });
+    // }
+
+    if (capturedImage) {
+      const blobUrl = capturedImage;
+      const filename = 'capturedImage.png';
+
+      fetch(blobUrl)
+        .then((response) => response.blob())
+        .then((blob) => {
+          const file = new File([blob], filename);
+
+          const formData = new FormData();
+          formData.append('file', file);
+          formData.append('name', 'capturedImage.png');
+          formData.append('empName', 'akash');
+          formData.append('empId', 'SI006423');
+          formData.append('category', 'bowl');
+          formData.append('chunks', 1);
+          formData.append('chunk', 0);
+
+          return axios.post('http://10.175.206.38:3038/api/image-upload', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          });
+        })
+        .then((response) => {
+          console.log('Image uploaded successfully:', response.data);
+          // Perform any additional actions after successful upload
+        })
+        .catch((error) => {
+          console.error('Error uploading image:', error);
+          // Handle upload error
+        });
+    }
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Webcam audio={false} ref={webcamRef} mirrored={facingMode === 'user'} />
+
+      <button onClick={captureImage}>Capture Image</button>
+      <button onClick={switchCamera}>Switch Camera</button>
+
+      {capturedImage && (
+        <div>
+          <h2>Captured Image:</h2>
+          <img src={capturedImage} alt="Captured" />
+          <button id="upload-button" onClick={handleUpload}>
+            Upload
+          </button>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default App
